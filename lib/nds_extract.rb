ec2160bbdd4495c5f3b99bf92d4c3911cbfb951d
Jack Cole <123jack.cole@gmail.com>
2020-01-14 22:33:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -16,17 +16,24 @@ end
 # using director_data as input
 def gross_for_director(director_data)
 
-  outer_index = 0
-  while outer_index < director_data.count do
-    name = director_data[outer_index][:name]
+  grand_total = 0
+row_index = 0
+while row_index < vm.length do
+  column_index = 0
+  while column_index < vm[row_index].length do
+    inner_len = vm[row_index][column_index].length
     inner_index = 0
-    gross_total = 0
-    while inner_index < director_data[outer_index][:movies].count do
-      gross_total += director_data[outer_index][:movies][inner_index][:worldwide_gross]
+    while inner_index < inner_len do
+      # Explanation!
+      # vm[row][column][spinner]
+      # spinner is full of Hashes with keys :price and :name
+      grand_total += vm[row_index][column_index][inner_index][:price]
       inner_index += 1
     end
-    outer_index += 1
+    column_index += 1
   end
+  row_index += 1
+end
 
 end
 
