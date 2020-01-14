e08abb1db6794631719be68f893e4a1a595a4e66
Jack Cole <123jack.cole@gmail.com>
2020-01-14 22:33:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,23 +17,23 @@ end
 def gross_for_director(director_data)
 
   grand_total = 0
-row_index = 0
-while row_index < vm.length do
-  column_index = 0
-  while column_index < vm[row_index].length do
-    inner_len = vm[row_index][column_index].length
-    inner_index = 0
-    while inner_index < inner_len do
-      # Explanation!
-      # vm[row][column][spinner]
-      # spinner is full of Hashes with keys :price and :name
-      grand_total += vm[row_index][column_index][inner_index][:price]
-      inner_index += 1
+  row_index = 0
+  while row_index < vm.length do
+    column_index = 0
+    while column_index < vm[row_index].length do
+      inner_len = vm[row_index][column_index].length
+      inner_index = 0
+      while inner_index < inner_len do
+        # Explanation!
+        # vm[row][column][spinner]
+        # spinner is full of Hashes with keys :price and :name
+        grand_total += vm[row_index][column_index][inner_index][:price]
+        inner_index += 1
+      end
+      column_index += 1
     end
-    column_index += 1
+    row_index += 1
   end
-  row_index += 1
-end
 
 end
 
