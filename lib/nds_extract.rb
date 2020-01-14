f203272752c49be9a187322686b48ba6b9bb4301
Jack Cole <123jack.cole@gmail.com>
2020-01-14 22:06:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,7 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
-
+grand_total = 0
+row_index = 0
+while row_index < vm.length do
+  column_index = 0
+  while column_index < vm[row_index].length do
+    grand_total += total_value_of_spinner(vm, row_index, column_index)
+    column_index += 1
+  end
+  row_index += 1
+end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
